00de0612aa1479e54b2b7968afe345b819a4f0f1
LKHZ <leta6603@gmail.com>
2020-01-14 14:48:02 +0900

    set remote repository e Please enter the commit message for your changes. Lines starting

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
-
-
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            // true를 반환하면 editing이 끝남 (end editing). -> 키보드를 없앰
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            // 키보드를 숨기지 않음
+            return false
+        }
+    }
+    
+    // textfield.endEditing(true) 호출시 아래 method가 호출된다.
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Use searchTextField.text to get the weather for that city.0
+        searchTextField.text = ""
+    }
 }
 
